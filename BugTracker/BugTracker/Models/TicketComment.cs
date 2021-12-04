00b3f785848bad794d9efeca8ad0dfb97039e147
Kyle Givler <xvegan88x@gmail.com>
2021-12-03 19:36:32 -0500

    some more models IG

---
+++ BugTracker/BugTracker/Models/TicketComment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace BugTracker.Models
+{
+    public class TicketComment
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Member Comment")]
+        public string Comment { get; set; }
+
+        [DisplayName("Date")]
+        public DateTimeOffset Created { get; set; }
+
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; }
+
+        [DisplayName("Team Member")]
+        public string Userid { get; set; }
+
+        ///////////////////////
+
+        public virtual Ticket Ticket { get; set; }
+
+        public virtual BTUser User { get; set; }
+    }
+}
